be823c3b674d5b23c2319189f087fc21dae59905
Danny Chang <dannychachang@gmail.com>
2018-02-14 14:59:55 -0500

    Finish part 1 of bikini bottom

--- election.js
+++ election.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // Imagination!
+  var candidateList = document.querySelector('.candidate-list')
+
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(responseData){
+    console.log(responseData);
+    responseData.candidates.forEach(function(candidate){
+      console.log(candidate.name);
+      console.log(candidate.votes);
+      var listEle = document.createElement('li');
+      listEle.innerHTML = ('Name: ' + candidate.name + ' Votes: ' + candidate.votes);
+      candidateList.append(listEle);
+    })
+  })
 
 });
